769f3a2ab825ab5210eae100c08fe7d5f3757da4
Lalit Sheoran <56827258+lalitsheoran@users.noreply.github.com>
2020-01-04 22:12:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getData() {
     }
 
     var request = $.ajax({
-        url: "//www.apilayer.net/api/live?access_key=2caa431a6bdc5af97697925c3198b02f&currencies=" + cFrom + "," + cTo + "&source=USD&format=1"
+        url: "https://apilayer.net/api/live?access_key=2caa431a6bdc5af97697925c3198b02f&currencies=" + cFrom + "," + cTo + "&source=USD&format=1"
     })
     request.done(function (data) {
         console.log(data)
